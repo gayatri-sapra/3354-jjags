a1bc49ce7f264b08996cc826d2218707ec31e936
Sulemanitmer <sulemanitmer@gmail.com>
2019-12-01 18:19:51 -0600

    Changed the size of the input. Changed textbox of vet info. Used Share Preferences to save medical and profile data

--- PetNannyDEV/app/src/main/java/com/example/petnannydev/ui/home/HomeFragment.java
+++ PetNannyDEV/app/src/main/java/com/example/petnannydev/ui/home/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.petnannydev.ui.home;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,10 +33,18 @@ public class HomeFragment extends Fragment {
     Button button;
     private static final int PICK_IMAGE = 100;
     Uri imageUri;
-    EditText name;
-    EditText owner;
-    EditText age;
-    EditText breed;
+    EditText petName;
+    EditText petOwner;
+    EditText petAge;
+    EditText petBreed;
+    EditText petBirth;
+
+    public static final String LAST_TEXT1 = "petName";
+    public static final String LAST_TEXT2 = "petOwner";
+    public static final String LAST_TEXT3 = "petAge";
+    public static final String LAST_TEXT4 = "petBreed";
+    public static final String LAST_TEXT5 = "petBirth";
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -50,12 +62,84 @@ public class HomeFragment extends Fragment {
         }
         });
 
-        name = (EditText) root.findViewById(R.id.nameInput);
-        owner = (EditText) root.findViewById(R.id.ownerInput);
-        age = (EditText) root.findViewById(R.id.ageInput);
-        breed = (EditText) root.findViewById(R.id.breedInput);
+        petName = (EditText) root.findViewById(R.id.nameInput);
+        petOwner = (EditText) root.findViewById(R.id.ownerInput);
+        petAge = (EditText) root.findViewById(R.id.ageInput);
+        petBreed = (EditText) root.findViewById(R.id.breedInput);
+        petBirth = (EditText) root.findViewById(R.id.birthdayInput);
+
+        final SharedPreferences hPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        petName.setText(hPref.getString(LAST_TEXT1, ""));
+        petOwner.setText(hPref.getString(LAST_TEXT2, ""));
+        petAge.setText(hPref.getString(LAST_TEXT3, ""));
+        petBreed.setText(hPref.getString(LAST_TEXT4, ""));
+        petBirth.setText(hPref.getString(LAST_TEXT5, ""));
+
+        petName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                hPref.edit().putString(LAST_TEXT1, s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        petOwner.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                hPref.edit().putString(LAST_TEXT2, s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        petAge.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                hPref.edit().putString(LAST_TEXT3, s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        petBreed.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                hPref.edit().putString(LAST_TEXT4, s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        petBirth.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                hPref.edit().putString(LAST_TEXT5, s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
         return root;
     }
+
+
 
     public void openGallery(){
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
@@ -69,7 +153,7 @@ public class HomeFragment extends Fragment {
         if(resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
             imageUri = data.getData();
             profilePic.setImageURI(imageUri);
-            name.setText(name.getEditableText(), TextView.BufferType.SPANNABLE);
+            petName.setText(petName.getEditableText(), TextView.BufferType.SPANNABLE);
         }
 
 
--- PetNannyDEV/app/src/main/java/com/example/petnannydev/ui/medical_history/MedicalHistoryFragment.java
+++ PetNannyDEV/app/src/main/java/com/example/petnannydev/ui/medical_history/MedicalHistoryFragment.java
@@ -1,9 +1,14 @@
 package com.example.petnannydev.ui.medical_history;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -18,6 +23,25 @@ public class MedicalHistoryFragment extends Fragment {
 
     private MedicalHistoryViewModel medicalHistoryViewModel;
 
+
+    EditText petConditions;
+    EditText vetName;
+    EditText vetAddress;
+    EditText vetPhone;
+    EditText petGshots;
+    EditText petPshots;
+    EditText petFood;
+    EditText petGroom;
+
+    public static final String LAST_TEXT6 = "petConditions";
+    public static final String LAST_TEXT7 = "vetName";
+    public static final String LAST_TEXT8 = "vetAddress";
+    public static final String LAST_TEXT9 = "vetPhone";
+    public static final String LAST_TEXT10 = "petGshots";
+    public static final String LAST_TEXT11 = "petPshots";
+    public static final String LAST_TEXT12 = "petFood";
+    public static final String LAST_TEXT13 = "petGroom";
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         medicalHistoryViewModel =
@@ -31,9 +55,123 @@ public class MedicalHistoryFragment extends Fragment {
             }
         });
 
+        petConditions = (EditText) root.findViewById(R.id.petConditions);
+        vetName = (EditText) root.findViewById(R.id.vetName);
+        vetAddress = (EditText) root.findViewById(R.id.vetAddress);
+        vetPhone = (EditText) root.findViewById(R.id.vetPhoneNumber);
+        petGshots = (EditText) root.findViewById(R.id.givenShots);
+        petPshots = (EditText) root.findViewById(R.id.pendingShots);
+        petFood = (EditText) root.findViewById(R.id.foodConsumptions);
+        petGroom = (EditText) root.findViewById(R.id.grooming);
+
+        final SharedPreferences mPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        petConditions.setText(mPref.getString(LAST_TEXT6, ""));
+        vetName.setText(mPref.getString(LAST_TEXT7, ""));
+        vetAddress.setText(mPref.getString(LAST_TEXT8, ""));
+        vetPhone.setText(mPref.getString(LAST_TEXT9, ""));
+        petGshots.setText(mPref.getString(LAST_TEXT10, ""));
+        petPshots.setText(mPref.getString(LAST_TEXT11, ""));
+        petFood.setText(mPref.getString(LAST_TEXT12, ""));
+        petGroom.setText(mPref.getString(LAST_TEXT13, ""));
+
+
+        petConditions.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPref.edit().putString(LAST_TEXT6, s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        vetName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPref.edit().putString(LAST_TEXT7, s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        vetAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPref.edit().putString(LAST_TEXT8, s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        vetPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPref.edit().putString(LAST_TEXT9, s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        petGshots.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPref.edit().putString(LAST_TEXT10, s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        petPshots.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPref.edit().putString(LAST_TEXT11,s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        petFood.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPref.edit().putString(LAST_TEXT12,s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        petGroom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPref.edit().putString(LAST_TEXT13,s.toString()).commit();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
         return root;
     }
-
-
-
 }
