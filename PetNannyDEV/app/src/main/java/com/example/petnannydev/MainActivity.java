015ee72e9281b30bb77fa1cb9c89fc9ee6eba37d
Jason Dang <jxd170830@utdallas.edu>
2019-12-02 16:00:24 -0600

    quality of life changes and added button in calendarView

--- PetNannyDEV/app/src/main/java/com/example/petnannydev/MainActivity.java
+++ PetNannyDEV/app/src/main/java/com/example/petnannydev/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.petnannydev;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,6 +13,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+
+
+    }
+
+    public void eventPage(View view){
+
     }
 
 }
