28bee0fe8f6244263dc54117fcae274da843dffc
Jason Dang <jxd170830@utdallas.edu>
2019-12-02 22:20:55 -0600

    Added another activity for a to do list

--- PetNannyDEV/app/src/main/java/com/example/petnannydev/ui/calender/CalenderFragment.java
+++ PetNannyDEV/app/src/main/java/com/example/petnannydev/ui/calender/CalenderFragment.java
@@ -1,22 +1,26 @@
 package com.example.petnannydev.ui.calender;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.petnannydev.R;
 
-public class CalenderFragment extends Fragment {
+public class CalenderFragment extends AppCompatActivity{
 
     private CalenderViewModel calenderViewModel;
+    private Button eventButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,5 +28,24 @@ public class CalenderFragment extends Fragment {
                 ViewModelProviders.of(this).get(CalenderViewModel.class);
         View root = inflater.inflate(R.layout.fragment_calender, container, false);
         return root;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        eventButton = (Button) findViewById(R.id.eventButton);
+        eventButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openToDoList();
+            }
+        });
+    }
+
+    public void openToDoList(){
+        Intent intent = new Intent(this, ToDoList.class);
+        startActivity(intent);
     }
 }
---
+++ PetNannyDEV/app/src/main/java/com/example/petnannydev/ui/calender/ToDoList.java
@@ -0,0 +1,16 @@
+package com.example.petnannydev.ui.calender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.petnannydev.R;
+
+public class ToDoList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do_list);
+    }
+}
