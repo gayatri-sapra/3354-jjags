41672983dda21cccf874951d03f198cc8852be5c
Suleman Itmer <sulemanitmer@gmail.com>
2019-12-03 20:06:16 -0600

    Made calender linked with the google Calender

--- PetNannyDEV/app/src/main/java/com/example/petnannydev/ui/calender/CalenderFragment.java
+++ PetNannyDEV/app/src/main/java/com/example/petnannydev/ui/calender/CalenderFragment.java
@@ -2,6 +2,7 @@ package com.example.petnannydev.ui.calender;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,19 +21,44 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.petnannydev.R;
 
+import java.util.GregorianCalendar;
+
 public class CalenderFragment extends Fragment{
 
-    EditText itme
+    private static final String TAG = "CalendarActivity";
 
-    TextView date_view;
-    View root;
+    private CalenderViewModel calenderViewModel;
+    private CalendarView mCalendarView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         calenderViewModel =
                 ViewModelProviders.of(this).get(CalenderViewModel.class);
-        root = inflater.inflate(R.layout.fragment_calender, container, false);
+        View root = inflater.inflate(R.layout.fragment_calender, container, false);
 
+        mCalendarView = (CalendarView) root.findViewById(R.id.calendarView);
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+
+                String date = i + "/" + i1 + "/" + i2;
+                Log.d(TAG,"onSelectedDayChange: mm/dd/yyyy: " + date);
+
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+
+
+                GregorianCalendar calDate = new GregorianCalendar(i, i1, i2);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        calDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        calDate.getTimeInMillis());
+
+                startActivity(calIntent);
+            }
+
+        });
         return root;
 
     }
--- Test/app/src/main/java/com/example/test/CalendarActivity.java
+++ Test/app/src/main/java/com/example/test/CalendarActivity.java
@@ -2,12 +2,16 @@ package com.example.test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.util.Log;
 import android.widget.CalendarView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class CalendarActivity extends AppCompatActivity {
     private static final String TAG = "CalendarActivity";
@@ -26,9 +30,20 @@ public class CalendarActivity extends AppCompatActivity {
                 String date = i + "/" + i1 + "/" + i2;
                 Log.d(TAG,"onSelectedDayChange: mm/dd/yyyy: " + date);
 
-                Intent intent = new Intent(CalendarActivity.this, DateActivity.class);
-                intent.putExtra("date", date);
-                startActivity(intent);
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "My House Party");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "My Beach House");
+                calIntent.putExtra(CalendarContract.Events.DESCRIPTION, "A Pig Roast on the Beach");
+
+                GregorianCalendar calDate = new GregorianCalendar(i, i1, i2);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        calDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        calDate.getTimeInMillis());
+
+                startActivity(calIntent);
             }
 
         });
